f615b94a065fcd0621afdc030a34dbcd8d11e901
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-12-21 16:22:04 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,13 @@
 class Author
- 
   attr_accessor :name
  
   def initialize(name)
     @name = name
+    @posts = []
+  end
+ 
+  def add_post(post)
+    @post << post
   end
 end
-
-charles_dickens = Author.new("Charles Dickens")
 
